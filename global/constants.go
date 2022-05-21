7f4c670eca936a0e2803a5945c00d8bcc44730db
Murat Şahin <murat.sanih@gmail.com>
2022-05-21 17:14:51 +0300

    ucarion/urlpath package added allowed list moved to global/constants.go

---
+++ global/constants.go
@@ -0,0 +1,15 @@
+package global
+
+import "github.com/ucarion/urlpath"
+
+// AllowedRoutes are the paths that sidecar-proxy application supports
+var AllowedRoutes = []urlpath.Path{
+	urlpath.New("company"),
+	urlpath.New("company/:id"),
+	urlpath.New("/company/account"),
+	urlpath.New("/account"),
+	urlpath.New("/account/:id"),
+	urlpath.New("/:id"),
+	urlpath.New("/account/:id/user"),
+	urlpath.New("/tenant/account/blocked"),
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/gofiber/fiber/v2 v2.33.0
 	github.com/stretchr/testify v1.7.1
+	github.com/ucarion/urlpath v0.0.0-20200424170820-7ccc79b76bbb
 )
 
 require (
