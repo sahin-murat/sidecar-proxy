1ffddacdee4da132da531e7e13a1bf4d00d283a8
Murat Şahin <murat.sanih@gmail.com>
2022-05-21 13:20:22 +0300

    Fiber added for http request handling

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/gofiber/fiber/v2"
+
 var allowedList []string
 
 func main() {
@@ -12,6 +14,13 @@ func main() {
 		"/{id}",
 		"/account/{id}/user",
 		"/tenant/account/blocked",
+	}
+
+	sideCarApplication := fiber.New()
+
+	err := sideCarApplication.Listen(":333")
+	if err != nil {
+		panic(err)
 	}
 }
 
